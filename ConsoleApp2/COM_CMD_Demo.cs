2a00acd607530ec174eb182eb1fa9144bd742c2c
bbsmith24 <bbsmith024@gmail.com>
2026-02-05 07:42:35 -0500

    Add project files.

---
+++ ConsoleApp2/COM_CMD_Demo.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+
+namespace ConsoleApp2
+{
+    internal class COM_CMD_Demo
+    {
+        static void Main(string[] args)
+        {
+            // list available serial ports
+            Console.WriteLine("Available Ports");
+            String[] ports = System.IO.Ports.SerialPort.GetPortNames();
+            for (int i = 0; i < ports.Length; i++)
+            {
+                Console.WriteLine(ports[i]);
+            }
+            // set up port to read from - you'll need to change the port name and possibly baud rate to match
+            System.IO.Ports.SerialPort port = new SerialPort("COM17", 115200, Parity.None, 8, StopBits.One);
+            // open the serial port
+            port.Open();
+            // this will be the process that monitors the GPU
+            Process process = new Process();
+            process.StartInfo.FileName = "TimerConsole.exe";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.Start();
+
+            // run until the console closes
+            while (true)
+            {
+                // this is the string that will be read from COM or command
+                // '?' allows it to be null (empty)
+                String? read = "";
+                // wait for something on the port
+                if (port.BytesToRead > 0)
+                {
+                    // read up to the end of the line
+                    read = port.ReadLine();
+                    // output current system time (the timestamp) to console, followed by a <tab>
+                    Console.Write(System.DateTime.Now.ToString());
+                    Console.Write("\tCOM\t");
+                    // output the line read from the COM port
+                    Console.WriteLine(read);
+                }
+                // read a line from the process
+                read = process.StandardOutput.ReadLine();
+                if (read != null)
+                {
+                    Console.Write(System.DateTime.Now.ToString());
+                    Console.Write("\tCMD\t");
+                    Console.WriteLine(read);
+                }
+            }
+        }
+    }
+}
---
+++ TimerConsole/TimerConsole.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Timers;
+class TimerConsole
+{
+    private static System.Timers.Timer aTimer = new System.Timers.Timer(2000.0);
+
+    static void Main(string[] args)
+    {
+        SetTimer();
+
+        Console.WriteLine("\nPress the Enter key to exit the application...\n");
+        Console.WriteLine($"The application started at {DateTime.Now:HH:mm:ss.fff}");
+        Console.ReadLine(); // Keep the console app running
+
+        aTimer.Stop();
+        aTimer.Dispose();
+        Console.WriteLine("Terminating the application...");
+    }
+
+    private static void SetTimer()
+    {
+        // Create a timer with a two second interval.
+        aTimer = new System.Timers.Timer(2000);
+        // Hook up the Elapsed event for the timer.
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true; // Repeat the event every interval
+        aTimer.Enabled = true; // Start the timer
+    }
+
+    private static void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+        Console.WriteLine($"The Elapsed event was raised at {e.SignalTime:HH:mm:ss.fff}");
+        // Add your recurring task logic here (e.g., logging, checking a database)
+    }
+}
